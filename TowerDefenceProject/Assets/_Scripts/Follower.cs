70872d6734b04fd08cc43bf2a5723a046d176055
sbilgecaneryilmaz <sbilgecan@gmail.com>
2022-04-26 23:17:10 +0300

    tower target lock on and rotation

--- TowerDefenceProject/Assets/_Scripts/Follower.cs
+++ TowerDefenceProject/Assets/_Scripts/Follower.cs
@@ -4,14 +4,28 @@ using PathCreation;
 public class Follower : MonoBehaviour
 {
     public PathCreator pathCreator;
+    public EndOfPathInstruction end;
     public float movedSpeed = 5f;
     float distanceTravalled;
+    
+    void Awake()
+    {
+        pathCreator = GameObject.Find("Path").GetComponent<PathCreator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         distanceTravalled += movedSpeed * Time.deltaTime;
-        transform.position = pathCreator.path.GetPointAtDistance(distanceTravalled);
-        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravalled);
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravalled,end);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravalled,end);       
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("EndLine"))
+        {
+             Destroy(this.gameObject);
+        }
     }
 }
---
+++ TowerDefenceProject/Assets/_Scripts/tower_Lvl1.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class tower_Lvl1 : MonoBehaviour
+{
+    private Transform target;
+    public Transform partToRotate;
+    public float targetRange = 15f;
+    public float rotateSpeed = 10f;
+    public string enemyTag = "Enemy";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget",0f,0.5f);
+    }
+
+    void UpdateTarget(){
+        // gets all the targets with enemy tag
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy<shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= targetRange)
+        {
+            target = nearestEnemy.transform;
+        }else{
+            target = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) return;
+        
+        // target lock on
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * rotateSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f,rotation.y,0f);
+
+    }
+
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position,targetRange);
+    }
+}
